e6b35838ee7b9f8a008098fef1001dff270b2f36
Antoine Piguet <pn01kbc@eduvaud.ch>
2024-09-27 16:39:02 +0200

    Commit ameliorations
    ajout de hitbox pour les ennemis, ajout de destruction des objets ennemis et projectiles et restructuration légère du code

---
+++ exos/Drones/Drones/TestProject1/UnitTest1.cs
@@ -0,0 +1,17 @@
+using Drones;
+namespace TestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+         /*   Drone drone = new Drone();
+            try
+            {
+                if( == null) { }
+            }catch (Exception ex) { }*/
+        }
+    }
+}
